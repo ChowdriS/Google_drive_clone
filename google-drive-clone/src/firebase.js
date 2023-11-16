e3c4bcef79fb27b52a9fd308eba90482fd767e9f
ChowdriS <114232036+ChowdriS@users.noreply.github.com>
2023-11-16 20:30:12 +0530

    update

--- google-drive-clone/src/firebase.js
+++ google-drive-clone/src/firebase.js
@@ -1,23 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; 
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA7kbzAlM6SL6pccej8kpKWv1qknwajcEE",
-  authDomain: "auth-development-25270.firebaseapp.com",
-  projectId: "auth-development-25270",
-  storageBucket: "auth-development-25270.appspot.com",
-  messagingSenderId: "1044855922638",
-  appId: "1:1044855922638:web:d0fc4999990c00cd9a159f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA7kbzAlM6SL6pccej8kpKWv1qknwajcEE",
+//   authDomain: "auth-development-25270.firebaseapp.com",
+//   projectId: "auth-development-25270",
+//   storageBucket: "auth-development-25270.appspot.com",
+//   messagingSenderId: "1044855922638",
+//   appId: "1:1044855922638:web:d0fc4999990c00cd9a159f"
+// };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); 
